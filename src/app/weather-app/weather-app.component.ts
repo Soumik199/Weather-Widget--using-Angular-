9f735fab2725ab7fa76b76371a9f4c3ef30bcc8a
Soumik199 <Soumikroy519@gmail.com>
2022-03-02 01:37:15 +0530

    complete

--- src/app/weather-app/weather-app.component.ts
+++ src/app/weather-app/weather-app.component.ts
@@ -11,13 +11,28 @@ import{ WeatherdataService } from '../weatherdata.service'
 export class WeatherAppComponent implements OnInit{
  
   weatherInf:any;
+  
+  
   constructor(private weatherdata:WeatherdataService ){
 
      this.weatherdata.weatherInf().subscribe((data)=>{
        this.weatherInf = data
-       console.log(this.weatherInf);
+       //console.log(this.weatherInf);
+       //let p = this.weatherInf.weather[0].main;
+       //console.log(p);
+       this.weatherInf.temp_celcius = (this.weatherInf.main.temp - 273.15).toFixed(0);
+       this.weatherInf.temp_min = (this.weatherInf.main.temp_min - 273.15).toFixed(0);
+       this.weatherInf.temp_max = (this.weatherInf.main.temp_max - 273.15).toFixed(0);
+       let sunsetTime = new Date(this.weatherInf.sys.sunset * 1000);
+       this.weatherInf.sunset_time = sunsetTime.toLocaleTimeString();
+       let currentDate = new Date();
+       this.weatherInf.isDay = (currentDate.getTime() < sunsetTime.getTime());
      })
     }
     today: number = Date.now(); //Data pipe in the angular 
+
     ngOnInit(): void {}
+
+    
+      
 }
